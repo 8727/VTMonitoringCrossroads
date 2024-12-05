3659f11080b47330fb622ed50a7deea613b62435
Alex Li <5796630@mail.ru>
2024-12-05 18:17:56 +0300

    messagebit |= 1 << x;

--- VTMonitoringCrossroads/Timer.cs
+++ VTMonitoringCrossroads/Timer.cs
@@ -82,7 +82,7 @@ namespace VTMonitoringCrossroads
                     {
                         if (Service.statusTrafficLight[x])
                         {
-                            messagebit +=  1 << x;
+                            messagebit |= 1 << x;
                         }
                     }
                 }
